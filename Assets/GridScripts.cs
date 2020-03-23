08e9cb1f427b3aea2efcd0e6d278c77e2f50bee5
Ky <heovang097@gmail.com>
2020-03-23 08:31:32 +0700

    Initial commit

---
+++ Assets/GridScripts.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScripts : MonoBehaviour
+{
+    public Transform[,] grid;
+    public GameObject playGround;
+    public GameObject[] prefabs;
+    public int gridHeight;
+    public int gridWidth;
+
+    void createGrid()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        playGround = GameObject.FindGameObjectWithTag("playGround");
+        gridWidth = (int)playGround.transform.localScale.x;
+        gridHeight = (int)playGround.transform.localScale.y;
+        grid = new Transform[gridWidth, gridHeight];
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Tetris
+ Complete Tetris game.
